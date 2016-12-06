fc6c635611de0c689e9a17c3007f1f5c821e8888
jtr109 <lyp_login@outlook.com>
2016-12-06 14:41:42 +0800

    Success to POST one request, but fail with the second one

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -36,20 +37,34 @@ class NewVisitorTest(unittest.TestCase):
         # 代办事项显示了"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
 
+        # time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows)
         # )
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear in table -- its text was:\n%s" % (
+        #         table.text,
+        #     )
+        # )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框, 可以输入其他的代办事项
         # 她输入了 "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新, 她的清单中显示了这两个代办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL

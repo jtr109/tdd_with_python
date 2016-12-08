4f7fd279e499ff22e49a736c70ec5240647f4a5b
jtr109 <lyp_login@outlook.com>
2016-12-08 08:27:05 +0800

    Use Bootstrap to imporve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
-from django.test import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import unittest
 import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
